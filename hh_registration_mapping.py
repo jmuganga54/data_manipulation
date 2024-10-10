21f5c94e00275246bcbe6e5e4e108fcc40fb0ad8
robsonfvieira <robsonfeichasvieira@gmail.com>
2024-10-10 10:14:34 -0300

    Initial commit - XLSForm processing and loading data from Central

---
+++ hh_registration_mapping.py
@@ -0,0 +1,5 @@
+﻿yes_no = {'yes': 'Yes', 'no': 'No'}
+relationship_to_household = {'head': 'Head of Household / Self', '\twife ': 'Wife', 'child': 'Child', 'grandchild': 'GrandChild', 'sibling': 'Brother/Sister', 'r_others': 'Others'}
+age_of_others = {'less_than_month': '< 1 month', '2-11m': '2-11 months', '1-5y': '1-5 years', '18-49y': 'woman of childbearing age (18-49 years)', 'a_others': 'Others'}
+gender = {'male': 'Male', 'female': 'Female'}
+age_range = {'less_than_month': '< 1 month', '2-11m': '2-11 months', '1-5y': '1-5 years', '18-49y': 'woman of childbearing age (18-49 years)', 'a_others': 'Other Age Range'}
